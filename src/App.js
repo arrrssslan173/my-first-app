9537ac12672d912ec652ce91b2c59c349a4a096e
arrrssslan17373 <arslan.asif9211@gmail.com>
2020-07-01 16:18:05 +0500

    light component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Light from './Light.js';
 
 function App() {
   return (
     <div>
-      <h1>Hello Everyone</h1>
+      <light />
     </div>
   );
 }
---
+++ src/Light.js
@@ -0,0 +1,10 @@
+import React, { useState} from 'react';
+
+function light (){
+    const state = useState(true);
+    return(
+    <button>Switch Light</button>
+    )
+}
+
+export default Light;
