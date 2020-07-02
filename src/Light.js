5d0cef65747c27f9406e0ed116735bae606ac971
arrrssslan17373 <arslan.asif9211@gmail.com>
2020-07-02 18:20:29 +0500

    light.js
    no idea why light.js is not picking up in github.

--- src/Light.js
+++ src/Light.js
@@ -3,16 +3,27 @@ import React, { useState } from 'react';
 
 function Light () {
     
-    let [isLit, setLit] = useState(false)
-
+    let [isLit, setLit] = useState(false);
+    let [color1, setColor1] = useState(0);
+    
     function updateLit(){
         setLit(!isLit);     
     }
+
+    function color(){
+        setColor1(!color1);
+    }
+
+
     return (
     <div>
         <h1>The Room is:  {isLit ? 'Lit' : 'Dark'} </h1>
+        <h1>The Bulb Color is: {color1 ? 'Yellow' : 'White'} </h1>
+
 
         <button className='btn2' onClick={updateLit}>Switch Light</button>
+        <br />
+        <button className='btn2' onClick={color}>Light Color</button>
     </div>
     );
 }
