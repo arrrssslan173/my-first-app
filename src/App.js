037a0d2d0d8d594e76eb38867ce2fc194d22bde5
arrrssslan17373 <arslan.asif9211@gmail.com>
2020-07-03 16:21:29 +0500

    calculator file seperated

--- src/App.js
+++ src/App.js
@@ -1,88 +1,16 @@
 import React, {useState} from 'react';
 import './App.css';
 import Light from './light.js';
+import Calculator from './calculator.js';
 
-function App() {
-
-  let [number, setNumber] = useState(0)
-
-  return (
+function App(){
+    return (
     <div>
-      <h1>Calculator</h1>
-      <h1>The Primary Number is = {number}</h1>
-      <button className="btn" onClick={
-        ()=> setNumber(number+1)
-        }>
-          Add One
-      </button>
-      
-      <button className="btn" onClick={
-        ()=> setNumber(number+2)
-        }>
-          Add Two
-      </button>
-
-      <button className="btn" onClick={
-        ()=> setNumber(number+3)
-        }>
-          Add Three
-      </button>
-
-      <button className="btn" onClick={
-        ()=> setNumber(number+4)
-        }>
-          Add Four
-      </button>
-      
-      <button className="btn" onClick={
-        ()=> setNumber(number+5)
-        }>
-          Add Five
-      </button>
-
-      <button className="btn" onClick={
-        ()=> setNumber(number+6)
-        }>
-          Add Six
-      </button>
-
-      <button className="btn" onClick={
-        ()=> setNumber(number+7)
-        }>
-          Add Seven
-      </button>
-      
-      <button className="btn" onClick={
-        ()=> setNumber(number+8)
-        }>
-          Add Eight
-      </button>
-
-      <button className="btn" onClick={
-        ()=> setNumber(number+9)
-        }>
-          Add Nine
-      </button>
-
-      <button className="btn" onClick={
-        ()=> setNumber(number+0)
-        }>
-          Add Zero
-      </button>
-
-
-      <button className="btn" onClick={
-        ()=> setNumber(number+parseInt(prompt('Enter Custom Number:')))
-        }>
-          Add Custom #
-      </button>
-
-      <hr />
-
-      <h1>Light Component</h1>
+      <Calculator />
       <Light />
     </div>
   );
 }
+    
 
 export default App;
