ad9937036b5c1137730d05d8be0898bbac4820fc
arrrssslan17373 <arslan.asif9211@gmail.com>
2020-07-13 20:19:38 +0500

    a

--- src/components/addTransactions.js
+++ src/components/addTransactions.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddTransaction() {
+
+    const [description, setDescription] = useState();
+    const [transactionAmount, setTransactionAmount] = useState();
+
+
     return(
         <div className='header'>
             <h4>Add Transaction</h4>
             <div className='lbl'>
                 <label>Description</label>
-                    <input className='input' placeholder='Detail of Transaction' type='text' required />
+                    <input className='input' 
+                    placeholder='Detail of Transaction' 
+                    type='text' required
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value) }
+                    />
+
                 <label>Amount</label>
-                    <input className='input' placeholder='Enter Amount' type='number' required />
+                    <input className='input' 
+                    placeholder='Enter Amount' 
+                    type='number' required
+                    value={transactionAmount} 
+                    onChange={(e) => setTransactionAmount(e.target.value) }
+                    />
             </div>
             <br /><br />
             <button className='btn' >Add Transaction</button>
