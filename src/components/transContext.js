358b7a9c443ee5aea17b4aaf14fadf9437534aa5
arrrssslan17373 <arslan.asif9211@gmail.com>
2020-07-13 20:21:16 +0500

    dd

--- src/components/transContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-const transactions = [
-    {desc: 'Salary', amount: '1000'},
-    {desc: 'Rent', amount: '-100'},
-    {desc: 'Gas', amount: '-100'}
-]
-
-export const TransContext = createContext{Transactions};
